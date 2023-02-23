479c56063ee7102a9a2d4228fa8e35f7f40d1855
peng-u-0807-macbook <penguin0807@snu.ac.kr>
2023-02-23 21:56:31 +0900

    create project

---
+++ Emotilt/Emotilt/EmotiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotiltApp.swift
+//  Emotilt
+//
+//  Created by peng on 2023/02/23.
+//
+
+import SwiftUI
+
+@main
+struct EmotiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
